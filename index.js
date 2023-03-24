cd77d735190cf4bd9c24da59153cad2eba3b318d
macrodatum <macrodatum@gmail.com>
2023-03-23 23:00:25 -0500

    refactorizacion de codigo

--- index.js
+++ index.js
@@ -1,293 +1,180 @@
-const express = require('express')
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 var url = require("url");
 var path = require("path");
-const venom = require('venom-bot');
-const axios = require('axios');
-const app = express()
-var bodyParser = require('body-parser')
-var https = require('https');
+const venom = require("venom-bot");
+const app = express();
+var bodyParser = require("body-parser");
+var https = require("https");
+var processMessage = require("./messageProcessor");
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-const port = 8080
+const port = 8030;
 
 let clientW = null;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.post('/ping', jsonParser, (req, res)=>{
+app.post("/ping", jsonParser, (req, res) => {
   try {
     let wsId = req.body.id;
-    let message = "ping "+(new Date()).getTime();
+    let message = "ping " + new Date().getTime();
     let typeMessage = req.body.type;
     switch (typeMessage) {
       case "text":
-        clientW.sendText(wsId+'@c.us', message).then((result)=>{
-          console.log(result)
-        })
+        clientW.sendText(wsId + "@c.us", message).then((result) => {
+          console.log(result);
+        });
         break;
-    
+
       default:
-        clientW.sendText(wsId+'@c.us', "unsupportedType: " + typeMessage +" [text, btn, link, imgb64]:: "+ message).then((result)=>{
-          console.log(result)
-        })
+        clientW
+          .sendText(
+            wsId + "@c.us",
+            "unsupportedType: " +
+              typeMessage +
+              " [text, btn, link, imgb64]:: " +
+              message
+          )
+          .then((result) => {
+            console.log(result);
+          });
         break;
     }
-    res.sendStatus(200)
+    res.sendStatus(200);
   } catch (error) {
-      console.log(error)
-      res.sendStatus(400)
+    console.log(error);
+    res.sendStatus(400);
   }
-})
+});
 
-app.post('/send', jsonParser, (req, res)=>{
+app.post("/send", jsonParser, (req, res) => {
   try {
     let wsId = req.body.id;
     let message = req.body.message;
     let typeMessage = req.body.type;
     let imageUrl = req.body.imageUrl;
     let imageName = req.body.imageName;
-    let fileUriTemp = ""
+    let fileUriTemp = "";
     switch (typeMessage) {
       case "text":
-        clientW.sendText(wsId+'@c.us', message).then((result)=>{
-          console.log("send text :: "+result)
-        })
+        clientW.sendText(wsId + "@c.us", message).then((result) => {
+          console.log("send text :: " + JSON.stringify(result));
+        });
         break;
       case "image":
-        downloadImage(imageUrl).then((fileUri)=>{
-          fileUriTemp = fileUri
+        downloadImage(imageUrl).then((fileUri) => {
+          fileUriTemp = fileUri;
           clientW
-          .sendImage(
-            wsId+'@c.us',
-            fileUri,
-            imageName,
-            message
+            .sendImage(wsId + "@c.us", fileUri, imageName, message)
+            .then((result) => {
+              deleteImage(fileUriTemp);
+              fileUriTemp = "";
+              console.log("Result image :: ", result); //return object success
+            })
+            .catch((erro) => {
+              console.error("Error when sending: ", erro); //return object error
+            });
+        });
+        break;
+
+      default:
+        clientW
+          .sendText(
+            wsId + "@c.us",
+            "unsupportedType: " +
+              typeMessage +
+              " [text, btn, link, imgb64]:: " +
+              message
           )
           .then((result) => {
-            deleteImage(fileUriTemp);
-            fileUriTemp = "";
-            console.log('Result image :: ', result); //return object success
-          })
-          .catch((erro) => {
-            console.error('Error when sending: ', erro); //return object error
+            console.log("unknow message type :: " + result);
           });
-        })
-        break;
-    
-      default:
-        clientW.sendText(wsId+'@c.us', "unsupportedType: " + typeMessage +" [text, btn, link, imgb64]:: "+ message).then((result)=>{
-          console.log("unknow message type :: "+result)
-        })
         break;
     }
-    res.sendStatus(200)
+    res.sendStatus(200);
   } catch (error) {
-      console.log(error)
-      res.sendStatus(400)
+    console.log(error);
+    res.sendStatus(400);
   }
-})
-  
+});
+
 app.listen(port, () => {
   venom
-  .create(
-  'charmss-notifications',
-  (base64Qr, asciiQR, attempts, urlCode) => {
-    console.log(asciiQR); // Optional to log the QR in the terminal
-    console.log("urlCode", urlCode)
-    var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-      response = {};
+    .create(
+      "charmss-notifications",
+      (base64Qr, asciiQR, attempts, urlCode) => {
+        console.log(asciiQR); // Optional to log the QR in the terminal
+        console.log("urlCode", urlCode);
+        var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+          response = {};
 
-    if (matches.length !== 3) {
-      return new Error('Invalid input string');
-    }
-    response.type = matches[1];
-    response.data = new Buffer.from(matches[2], 'base64');
-
-    var imageBuffer = response;
-    require('fs').writeFile(
-      './public/images/qr.png',
-      imageBuffer['data'],
-      'binary',
-      function (err) {
-        if (err != null) {
-          console.log(err);
+        if (matches.length !== 3) {
+          return new Error("Invalid input string");
         }
-      }
-    );
-  },
-    // statusFind
-    (statusSession, session) => {
-      console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || initWhatsapp || erroPageWhatsapp || successPageWhatsapp || waitForLogin || waitChat || successChat
-      //Create session wss return "serverClose" case server for close
-      console.log('Session name: ', session);
-    },
-  { logQR: false }
-  )
-  .then((client) => {
+        response.type = matches[1];
+        response.data = new Buffer.from(matches[2], "base64");
+
+        var imageBuffer = response;
+        require("fs").writeFile(
+          "./public/images/qr.png",
+          imageBuffer["data"],
+          "binary",
+          function (err) {
+            if (err != null) {
+              console.log(err);
+            }
+          }
+        );
+      },
+      // statusFind
+      (statusSession, session) => {
+        console.log("Status Session: ", statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || initWhatsapp || erroPageWhatsapp || successPageWhatsapp || waitForLogin || waitChat || successChat
+        //Create session wss return "serverClose" case server for close
+        console.log("Session name: ", session);
+      },
+      { logQR: false }
+    )
+    .then((client) => {
       clientW = client;
-      start(client)
-      console.log(client);
-  })
-  .catch((erro) => {
+      start(client);
+      console.log("charmss-notifications started");
+    })
+    .catch((erro) => {
       console.log("Error generated for QRCode");
       console.log(erro);
-  });
-  
-    console.log(`Server on ${port}`)
-})
-
-  function start(client) {
-    let urlPerformer = "http://api.charmss.com/api/account/whatsapp"
-    let urlClient = "https://api.charmss.com/api/customer/whatsapp"
-    let urlClientCancel = "https://api.charmss.com/api/customer/whatsappcancel"
-
-    client.onMessage((message) => { 
-      console.log(message)   
-      if (message.body.includes('performer::') && message.isGroupMsg === false) {
-        axios.post(urlPerformer, {
-          whatsappId: message.body,
-        })
-        .then((response) => {
-          if(response.data.data === "ok"){
-            client
-            .sendImage(message.from, 
-              __dirname + "/public/images/logocharmss.jpg",
-              "logocharmss.jpg",
-              'CharmssBOT your number has been registed, please go to https://performers.charmss.com')
-            .then((result) => {
-              console.log('Result: ', result);
-            })
-            .catch((erro) => {
-              console.error('Error when sending: ', erro);
-            });
-          }
-          else{
-            client
-            .sendText(message.from, 'CharmssBOT: your whatsapp number is not registered in our system please contact our support team at support@livecharmss.com.co')
-            .then((result) => {
-              console.log('Result: ', result);
-            })
-            .catch((erro) => {
-              console.error('Error when sending: ', erro);
-            });
-          }
-
-        }, (error) => {
-          console.error(" Error validating whatsappId " + error);
-        });
-      }
-      else if(message.type == 'reply'){
-        let acceptKey = message.body.toLowerCase();
-        let quotedMsg = message.quotedMsg.caption;
-
-        if(!acceptKey.includes('accept')){
-          client
-            .sendText(message.from, 'Please Reply to the security message (client::XXXXX-XXXXXXXXXXX) that has come to your WhatsApp with the word accept. support@livecharmss.com.co')
-            .then((result) => {
-              console.log('Result: ', result);
-            })
-            .catch((erro) => {
-              console.error('Error when sending: ', erro);
-            });
-        }
-        else{
-          axios.post(urlClient, {
-            whatsappId: quotedMsg,
-          })
-          .then((response) => {
-            if(response.data.data === "ok"){
-              client
-              .sendImage(
-                message.from, 
-                __dirname + "/public/images/logocharmss.jpg",
-                "logocharmss.jpg",
-                'CharmssBOT your number has been registed, please go to https://www.livecharmss.com. if you want to delete this notifications send message at this contact with the word "cancel"')
-              .then((result) => {
-                console.log('Result: ', result);
-              })
-              .catch((erro) => {
-                console.error('Error when sending: ', erro);
-              });
-            }
-            else{
-              client
-              .sendText(message.from, 'CharmssBOT: your whatsapp number is not registered in our clients system please contact our support team at support@livecharmss.com.co')
-              .then((result) => {
-                console.log('Result: ', result);
-              })
-              .catch((erro) => {
-                console.error('Error when sending: ', erro);
-              });
-            }
-          }, (error) => {
-            console.error(" Error validating whatsappId " + error);
-          });
-        }
-      }
-      else if (message.body.toLowerCase().includes('accept') && message.isGroupMsg === false) {
-        client
-        .sendText(message.from, 'Please Reply to the security message (client::XXXXX-XXXXXXXXXXX) that has come to your WhatsApp from this contact with the word accept. support@livecharmss.com.co')
-        .then((result) => {
-          console.log('Result: ', result);
-        })
-        .catch((erro) => {
-          console.error('Error when sending: ', erro);
-        });
-      }
-      else if (message.body.toLowerCase().includes('cancel') && message.isGroupMsg === false) {
-        axios.post(urlClientCancel, {
-          whatsappId: message.from,
-        })
-        .then((response) => {
-          if(response.data.data === "ok"){
-            client
-            .sendText(message.from, 'CharmssBOT your whatsapp contact has been deleted from https://www.livecharmss.com')
-            .then((result) => {
-              console.log('Result: ', result);
-            })
-            .catch((erro) => {
-              console.error('Error when sending: ', erro);
-            });
-          }
-          else{
-            client
-            .sendText(message.from, 'CharmssBOT: your whatsapp number is not registered in our client system please contact our support team at support@livecharmss.com.co')
-            .then((result) => {
-              console.log('Result: ', result);
-            })
-            .catch((erro) => {
-              console.error('Error when sending: ', erro);
-            });
-          }
-        }, (error) => {
-          console.error(" Error validating whatsappId " + error);
-        });
-      }
     });
-  }
 
-  function downloadImage(urldata){
-    return new Promise((resolve, reject)=>{
-      var parsed = url.parse(urldata);
-      var name = path.basename(parsed.pathname)
-      const file = fs.createWriteStream(__dirname + "/temp/"+name);
-      const request = https.get(urldata, function(response) {
+  console.log(`Server on ${port}`);
+});
+
+function start(client) {
+  client.onMessage((message) => {
+    console.log(message);
+    processMessage(client, message);
+  });
+}
+
+function downloadImage(urldata) {
+  return new Promise((resolve, reject) => {
+    var parsed = url.parse(urldata);
+    var name = path.basename(parsed.pathname);
+    const file = fs.createWriteStream(__dirname + "/temp/" + name);
+    const request = https.get(urldata, function (response) {
       response.pipe(file);
       file.on("finish", () => {
-            file.close();
-            return resolve(__dirname + "/temp/"+name)
-        });
+        file.close();
+        return resolve(__dirname + "/temp/" + name);
       });
     });
-  }
-
-  function deleteImage(fileName){
-    fs.unlink(fileName, (err) => {
-      if (err) {
-          throw err;
-      }
   });
-  }
+}
+
+function deleteImage(fileName) {
+  fs.unlink(fileName, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+}
---
+++ messageProcessor.js
@@ -0,0 +1,163 @@
+const axios = require("axios");
+/*
+    client: el objeto de conexion de whatsapp
+    message: el objeto con el mensaje a procesar
+ */
+const processMessage = (client, message) => {
+  if (isReply(message)) {
+    if (message.quotedMsg.body.includes("client::")) {
+      processRequest(client, message, "client");
+    } else if (message.quotedMsg.body.includes("performer::")) {
+      processRequest(client, message, "performer");
+    } else {
+      client
+        .sendText(
+          message.from,
+          "LiveCharmss enjoy all experience for you, https://www.livecharmss.com"
+        )
+        .then((result) => {
+          console.log("Result: ", result);
+        })
+        .catch((erro) => {
+          console.error("Error when sending: ", erro);
+        });
+    }
+  } else if (isCancel(message) && message.isGroupMsg === false) {
+    processCancel(client, message);
+  } else if (ErrorRequestAccept(message) && message.isGroupMsg === false) {
+    client
+      .sendText(
+        message.from,
+        "Please Reply the security message (that contains a similar text AAAAAA::XXXXXXXXXXXXXXXX) that has come to your WhatsApp with the word accept. support@livecharmss.com.co"
+      )
+      .then((result) => {
+        console.log("Result: ", result);
+      })
+      .catch((erro) => {
+        console.error("Error when sending: ", erro);
+      });
+  }
+};
+
+const isReply = (message) => {
+  return message.type === "reply" ? true : false;
+};
+
+const isCancel = (message) => {
+  return message.body.toLowerCase().includes("cancel") ? true : false;
+};
+
+const ErrorRequestAccept = (message) => {
+  return message.body.toLowerCase().includes("accept") ? true : false;
+};
+
+const processRequest = (client, message, type) => {
+  let url = "";
+  let site = "";
+  if (type === "client") {
+    url = "https://api.charmss.com/api/customer/whatsapp";
+    site = "https://www.livecharmss.com";
+  } else {
+    url = "https://api.charmss.com/api/account/whatsapp";
+    site = "https://performer.charmss.com";
+  }
+
+  let acceptKey = message.body.toLowerCase();
+  let quotedMsg = message.quotedMsg.body;
+
+  if (!acceptKey.includes("accept")) {
+    client
+      .sendText(
+        message.from,
+        `Please Reply to the security message (${type}::XXXXX-XXXXXXXXXXX) that has come to your WhatsApp with the word accept. support@livecharmss.com.co`
+      )
+      .then((result) => {
+        console.log("Result: ", result);
+      })
+      .catch((erro) => {
+        console.error("Error when sending: ", erro);
+      });
+  } else {
+    axios
+      .post(url, {
+        whatsappId: message.from,
+        token: quotedMsg,
+      })
+      .then(
+        (response) => {
+          if (response.data.data === "ok") {
+            client
+              .sendImage(
+                message.from,
+                __dirname + "/public/images/logocharmss.jpg",
+                "logocharmss.jpg",
+                `CharmssBOT your number has been registed, please go to ${site}. if you want to delete this notifications send message at this contact with the word "cancel"`
+              )
+              .then((result) => {
+                console.log("Result: ", result);
+              })
+              .catch((erro) => {
+                console.error("Error when sending: ", erro);
+              });
+          } else {
+            client
+              .sendText(
+                message.from,
+                "CharmssBOT: your whatsapp number is not registered in our system please contact our support team at support@livecharmss.com.co"
+              )
+              .then((result) => {
+                console.log("Result: ", result);
+              })
+              .catch((erro) => {
+                console.error("Error when sending: ", erro);
+              });
+          }
+        },
+        (error) => {
+          console.error(" Error validating whatsappId " + error);
+        }
+      );
+  }
+};
+
+const processCancel = (client, message) => {
+  let urlClientCancel = "https://api.charmss.com/api/customer/whatsappcancel";
+  axios
+    .post(urlClientCancel, {
+      whatsappId: message.from,
+    })
+    .then(
+      (response) => {
+        if (response.data.data === "ok") {
+          client
+            .sendText(
+              message.from,
+              "CharmssBOT your whatsapp contact has been deleted from https://www.livecharmss.com"
+            )
+            .then((result) => {
+              console.log("Result: ", result);
+            })
+            .catch((erro) => {
+              console.error("Error when sending: ", erro);
+            });
+        } else {
+          client
+            .sendText(
+              message.from,
+              "CharmssBOT: your whatsapp number is not registered in our client system please contact our support team at support@livecharmss.com.co"
+            )
+            .then((result) => {
+              console.log("Result: ", result);
+            })
+            .catch((erro) => {
+              console.error("Error when sending: ", erro);
+            });
+        }
+      },
+      (error) => {
+        console.error(" Error validating whatsappId " + error);
+      }
+    );
+};
+
+module.exports = processMessage;
